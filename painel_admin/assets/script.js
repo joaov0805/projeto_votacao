6a1ba226029a988d316eaa51c4b004091006269e
Ylana <ylanatorres6@gmail.com>
2026-04-27 09:49:33 -0300

    grafico

--- painel_admin/assets/script.js
+++ painel_admin/assets/script.js
@@ -1,1 +1,34 @@
+const votos = {
+    "Luana Gonçalves": 35,
+    "Manoela Pessanha": 25,
+    "Alessandra Pereira": 15,
+    "Alice Reino": 10
+};
 
+// Converte para arrays
+const nomes = Object.keys(votos);
+const quantidade = Object.values(votos);
+
+// Contexto correto do canvas
+const ctx = document
+    .getElementById("graficoResultados")
+    .getContext("2d");
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: nomes,
+        datasets: [{
+            data: quantidade,
+            backgroundColor: [
+                '#ff9999',
+                '#66b3ff',
+                '#99ff99',
+                '#ffcc99'
+            ]
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
